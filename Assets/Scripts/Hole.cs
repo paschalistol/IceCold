9a16f84d0fe596676f5b21bb5d02b7590a4abeea
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2021-10-08 13:50:38 +0200

    pool

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
+    private SurvivalPool pool;
+    private bool survival;
+    private PoolObject type = PoolObject.BIG_HOLE;
+
     public virtual void BallInHole()
     {
         ClassicGameManager.instance.Die();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pool") && survival)
+        {
+            gameObject.SetActive(false);
+            pool.AddToPool(type, gameObject);
+        }
+    }
+    public void SurvivalHole(SurvivalPool survivalPool)
+    {
+        pool = survivalPool;
+        survival = true;
     }
 }
