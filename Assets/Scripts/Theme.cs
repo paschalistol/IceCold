fb86216cbc7f26fd7f4db32c0dc93e489b1a21e3
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2021-11-02 16:09:07 +0100

    start of themes

--- Assets/Scripts/Theme.cs
+++ Assets/Scripts/Theme.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Theme", menuName = "ScriptableObjects/Theme", order = 1)]
 public class Theme : ScriptableObject
 {
-    public Color ballColor = new Color(114,156,255);
-    public Color survivalBgColor = new Color(169,180,198);
+    public Color ballColor = new Color(114/255f,156/255f,255/255f);
+    public Color survivalBgColor = new Color(169/255f,180/255f,198/255f);
     public Sprite classicBg;
     public Color machine;
+    public Color bar = new Color(207/255f, 77/255f, 0);
+    public Sprite platform;
 }
